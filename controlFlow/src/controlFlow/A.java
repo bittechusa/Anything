51bfa361eac6ce3fa475f8e0d9f38d44a1bfd512
my name <you@examplejtge.com>
2019-04-17 19:46:23 -0400

    sfdsf

---
+++ controlFlow/src/controlFlow/A.java
@@ -0,0 +1,87 @@
+package controlFlow;
+
+public class A {
+	
+	public void m()
+	{
+		int a = 5;
+		if(a>3)
+			System.out.println("a is greater");
+		else if(a< 5)
+			System.out.println("a is not greater");
+		else if(a == 5)
+			System.out.println("a is not greater");
+		else if(a<=5)
+			System.out.println("a is not greater");
+		else
+			System.out.println();
+		
+	}
+	
+	public void m1()
+	{
+	String s = "fail";
+	
+	switch(s)
+	{
+	case "pass" :
+		System.out.println("pass");
+		break;
+		
+	case "fail" :
+		System.out.println("fail");
+		break;
+		
+	case "skip" :
+		System.out.println("skipped");
+		break;
+		
+	default :
+		System.out.println("hello");
+		break;
+
+	}
+	
+	
+	
+//	public void m3()
+//	{
+//		
+//		//while loop
+//		//while(day = weekend)
+//				//go to bittech;
+//		
+//		int a = 4 ;
+//		
+//		while (a>2)
+//			System.out.println();
+//		
+//		
+//		//do-while
+////		go to bittech
+////		while(day == weekend)
+//		
+//		
+//		
+//		
+//		
+//		
+//	}
+	
+	
+	
+	
+	
+	
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ controlFlow/src/controlFlow/B.java
@@ -0,0 +1,32 @@
+package controlFlow;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+	int a = 1 ;
+	
+		while (a<2) {
+			System.out.println("hi");
+			a++;
+		
+		}
+		
+		int b =5;
+		do {System.out.println("hello");}
+		while(b>10);
+		
+		
+		//for loop
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
